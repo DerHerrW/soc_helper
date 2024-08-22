c7c77b8b71b18075a99e8d1f59d8471457409020
DerHerrW <129861137+DerHerrW@users.noreply.github.com>
2024-08-22 15:11:56 +0200

    Update cars.py
    Lizenzheader zugefügt

--- cars.py
+++ cars.py
@@ -2,6 +2,21 @@
 cars.py
 Definiert die OBD2-Kommandos der zur Auswahl stehenden Fahrzeuge und baut aus den Angaben in configuration.py mit den OBD2-Kommandos
 für jeden Fahrzeugtyp eine Fahrzeugklasse zusammen.
+
+part of project soc_helper
+Copyright (C) 2023-2024  M. Williges (spam at zut punkt de)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from dataclasses import dataclass, field
